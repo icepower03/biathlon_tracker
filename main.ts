966a97b3c4c9bb0daac675319045c0346055fa1b
icepower03 <chabrol.louis@gmail.com>
2020-08-30 21:11:36 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 radio.setGroup(3);
 radio.setTransmitPower(7);
 serial.redirectToUSB();
-let i=0;
-radio.onReceivedString(function (receivedString: string) {
 
-    serial.writeLine(receivedString);
-  
-  
+radio.onReceivedString(function (receivedString: string) {
+    serial.writeLine(receivedString); 
 })
 input.onButtonPressed(Button.A, function () {
       serial.writeLine("test A");
